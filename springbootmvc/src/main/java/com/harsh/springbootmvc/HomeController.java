1d512218027e4fd158f1a5bd0d1bbcffff1f5b2c
RAVANv2 <chauhanharsh3363334@gmail.com>
2021-01-16 20:50:51 +0530

    spring web development

---
+++ springbootmvc/src/main/java/com/harsh/springbootmvc/HomeController.java
@@ -0,0 +1,65 @@
+package com.harsh.springbootmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String homePage()
+    {
+        System.out.println("Home Page is Running");
+        return "index";
+    }
+
+//    This Function takes the request comming from frontend and returns the result in jsp form
+    @RequestMapping("add")
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+//        Model and View class is responsible for returning your jsp file with some data
+        ModelAndView mv = new ModelAndView();
+//        Add prefix and suffix in appication.properties for adding path of jsp file
+        mv.setViewName("result");
+
+        int num3 = num1 + num2;
+        mv.addObject("num3",num3);
+        return mv;
+    }
+
+
+//    This Function also does same thing but have different syntax
+    /*
+    @RequestMapping("add")
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model m){
+        int num3 = num1 + num2;
+        m.addAttribute("num3",num3);
+        return "result";
+    }
+     */
+
+    // Complex way to write function and Efficient as well
+    @RequestMapping("addAlien")
+    public String addAlien(@ModelAttribute("alien") Alien a){
+        return "alien";
+    }
+
+    // Simple way but not satisfying coding standard
+    /*
+    @RequestMapping("addAlien")
+    public String addAlien(@RequestParam("id") int id, @RequestParam("name") String name, HttpSession session){
+        Alien a = new Alien();
+        a.setId(id);
+        a.setName(name);
+
+        session.setAttribute("alien",a);
+        return "alien";
+    }
+
+     */
+}
---
+++ springbootmvc/src/main/webapp/views/index.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8" isELIgnored="false" %>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Insert Title Here</title>
+    </head>
+
+    <body>
+        Welcome To Homepage!!
+        <br>
+        <br>
+        <form action="add">
+            Enter 1st Number: <input type="text" name="num1"> <br>
+            Enter 2nd Number: <input type="text" name="num2"> <br>
+            <input type="submit">
+        </form>
+
+        <br>
+        <br>
+
+        <form action="addAlien">
+            Enter User ID: <input type="text" name="id"> <br>
+            Enter User Name: <input type="text" name="name"> <br>
+            <input type="submit">
+        </form>
+    </body>
+
+</html>
